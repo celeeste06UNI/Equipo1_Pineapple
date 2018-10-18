05cda2c1293954878e3f22d3c7e4e20926deb1eb
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-10-18 10:27:45 +0200

    change in homecontroller

--- src/main/java/com/pineapple/intime/dao/DAOEmpleado.java
+++ src/main/java/com/pineapple/intime/dao/DAOEmpleado.java
@@ -7,6 +7,7 @@ import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 
 public class DAOEmpleado {
 
@@ -15,4 +16,21 @@ public class DAOEmpleado {
 		Document doc=new Document("idEmpleado","nombre").append(idEmpleado,empleado);    
 		collection.insertOne(doc);
 	}
+	public static ConcurrentHashMap<Integer, String> load() {
+		MongoCollection<Document> collection = MongoBroker.get().getCollection("Empleado");
+		ConcurrentHashMap<Integer, String> result=new ConcurrentHashMap<Integer, String>();
+		MongoCursor<Document> cursor = collection.find().iterator();
+		while (cursor.hasNext()) {
+			Document empleadodoc = cursor.next();
+			int _id=(int) empleadodoc.getDouble("_id").intValue();
+			String empleado=empleadodoc.getString("nombre");
+			result.put(_id, empleado);
+		}
+		return result;
+	}
+	public static Boolean autenticar(String email,String password) {
+		MongoCollection<Document> collection = MongoBroker.get().getCollection("Empleado");
+		collection.find("{email:{$exists:true}},{'email':'celeste@mail'");
+	}
+	
 }
