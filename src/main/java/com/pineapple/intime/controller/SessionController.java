fa569b819527f8f52a3d051744013e92e1ba2d85
celeste <celeste@192.168.1.73>
2018-10-23 12:29:00 +0200

    boton de crear

--- src/main/java/com/pineapple/intime/controller/SessionController.java
+++ src/main/java/com/pineapple/intime/controller/SessionController.java
@@ -1,8 +1,15 @@
 package com.pineapple.intime.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 public class SessionController {
 	
-
+	@RequestMapping(value = "/newUser", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView model) {
+		model.setViewName("newUser");
+		return model;
+	}
 
 }
