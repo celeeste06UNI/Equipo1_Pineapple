c70de603897753b51538949369f176fb3b61f523
celeste <celeste@192.168.1.73>
2018-10-23 12:47:11 +0200

    Usuario Controller

--- src/main/java/com/pineapple/intime/controller/UsuarioController.java
+++ src/main/java/com/pineapple/intime/controller/UsuarioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class SessionController {
+public class UsuarioController {
 	
 	@RequestMapping(value = "/newUser", method = RequestMethod.GET)
 	public ModelAndView newUser(ModelAndView model) {
