09dae5c2bd60204f35ad9100a51a0ef6924bdb3b
celeste <celeste@172.19.182.225>
2018-11-19 11:36:34 +0100

    searchFichaje

--- src/main/java/com/pineapple/intime/controller/FichajeController.java
+++ src/main/java/com/pineapple/intime/controller/FichajeController.java
@@ -61,7 +61,7 @@ public class FichajeController {
 		String email = (String) session.getAttribute("emailSession");
 		Document Document = DAOFichaje.consultarFichajes(email, fecha);
 		for(int i = 1; i<Document.keySet().size();i++) {
-			listDate.add(Document.get(""+i));
+			listDate.add(Document.get(i));
 		}
 		model.addObject("listDate", listDate);
 		model.setViewName("fichajeUser");
