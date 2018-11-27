9645bccfcab568b9bc4995f60e0f58c73b629e26
celeste <celeste@192.168.1.73>
2018-11-27 20:27:06 +0100

    usuario controller

--- src/main/java/com/pineapple/intime/controller/UsuarioController.java
+++ src/main/java/com/pineapple/intime/controller/UsuarioController.java
@@ -14,49 +14,152 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import com.pineapple.intime.dao.DAOEmpleado;
 import com.pineapple.intime.dominio.EmpleadoHelper;
 
 @Controller
 public class UsuarioController {
-	
+
 	@RequestMapping(value = "/newUser", method = RequestMethod.GET)
-	public ModelAndView newUser(ModelAndView model) {
-		model.setViewName("newUser");
+	public ModelAndView newUser(ModelAndView model, HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			model.setViewName("newUser");
+		}
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
-	public ModelAndView deleteUser(ModelAndView model) {
-		model.setViewName("deleteUser");
+	public ModelAndView deleteUser(ModelAndView model, HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			model.setViewName("deleteUser");
+		}
 		return model;
 	}
-		
+
 	@RequestMapping(value = "/actionDeleteUser", method = RequestMethod.POST)
-	public String actionDeleteUser(@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
+	public ModelAndView actionDeleteUser(ModelAndView model, HttpServletRequest request,
+			@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
 			@ModelAttribute("email") String email, @ModelAttribute("rol") String rol) {
-		Document empleado=new Document();
-		String emailLowerCase=email.toLowerCase();
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			Document empleado = new Document();
+			String emailLowerCase = email.toLowerCase();
+			empleado.put("email", emailLowerCase);
+			empleado.put("rol", rol);
+			empleado.put("nombre", nombre);
+			empleado.put("apellidos", apellidos);
+			DAOEmpleado.delete(empleado);
+			model.setViewName("admin");
+		}
+
+		return model;
+	}
+
+	@RequestMapping(value = "/updateUser", method = RequestMethod.GET)
+	public ModelAndView updateUser(HttpServletRequest request, ModelAndView model) {
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			model.setViewName("updateUser");
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/viewUpdatePassword", method = RequestMethod.GET)
+	public ModelAndView viewUpdatePassword(HttpServletRequest request, ModelAndView model) {
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			model.setViewName("updatePassword");
+		}
+
+		return model;
+	}
+
+	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
+	public ModelAndView saveUser(HttpServletRequest request, ModelAndView model,@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
+			@ModelAttribute("email") String email, @ModelAttribute("rol") String rol) throws Exception {
+		HttpSession session = request.getSession(true);
+		String rolSession = (String) session.getAttribute("rolSession");
+		if (rolSession.equals("admin")) {
+			Document empleado = new Document();
+			String contrasenna = EmpleadoHelper.generarContrasenna();
+			String emailLowerCase = email.toLowerCase();
+			empleado.put("email", emailLowerCase);
+			empleado.put("rol", rol);
+			empleado.put("nombre", nombre);
+			empleado.put("apellidos", apellidos);
+			empleado.put("contrasenna", contrasenna);
+			if (DAOEmpleado.insert(empleado)) {
+				EmpleadoHelper.sesionEmail(email, contrasenna);
+			}
+			model.setViewName("admin");
+		}
+		
+
+		return model;
+	}
+
+	@RequestMapping(value = "/editUser", method = RequestMethod.POST)
+	public String editUser(@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
+			@ModelAttribute("emailAntiguo") String emailAntiguo, @ModelAttribute("emailNuevo") String emailNuevo,
+			@ModelAttribute("rol") String rol) {
+		Document empleado = new Document();
+		String emailLowerCase = emailNuevo.toLowerCase();
 		empleado.put("email", emailLowerCase);
 		empleado.put("rol", rol);
 		empleado.put("nombre", nombre);
 		empleado.put("apellidos", apellidos);
-		DAOEmpleado.delete(empleado);
+		DAOEmpleado.update(emailAntiguo, empleado);
 		return "admin";
 	}
-	
-	@RequestMapping(value = "/updateUser", method = RequestMethod.GET)
-	public ModelAndView updateUser(ModelAndView model) {
-		model.setViewName("updateUser");
+
+	@RequestMapping(value = "/searchUser", method = RequestMethod.POST)
+	public ModelAndView searchUser(ModelAndView model, @ModelAttribute("email") String email) {
+		String emailLowerCase = email.toLowerCase();
+		try {
+			Document empleado = DAOEmpleado.cargarEmpleado(emailLowerCase);
+			model.addObject("nombre", empleado.get("nombre"));
+			model.addObject("apellidos", empleado.get("apellidos"));
+			model.addObject("email", empleado.get("email"));
+			model.addObject("rol", empleado.get("rol"));
+			model.setViewName("updateUser");
+		} catch (Exception e) {
+			model.addObject("nombre", "");
+			model.addObject("apellidos", "");
+			model.addObject("email", "");
+			model.addObject("rol", "");
+			model.setViewName("updateUser");
+		}
 		return model;
 	}
-	
-	@RequestMapping(value = "/viewUpdatePassword", method = RequestMethod.GET)
-	public ModelAndView viewUpdatePassword(ModelAndView model) {
-		model.setViewName("updatePassword");
+
+	@RequestMapping(value = "/deleteSearchUser", method = RequestMethod.POST)
+	public ModelAndView deleteSearchUser(ModelAndView model, @ModelAttribute("email") String email) {
+		Document empleado = new Document();
+		String emailLowerCase = email.toLowerCase();
+		try {
+			empleado = DAOEmpleado.cargarEmpleado(emailLowerCase);
+			model.addObject("nombre", empleado.get("nombre"));
+			model.addObject("apellidos", empleado.get("apellidos"));
+			model.addObject("email", empleado.get("email"));
+			model.addObject("rol", empleado.get("rol"));
+			model.setViewName("deleteUser");
+		} catch (Exception e) {
+			model.addObject("nombre", "");
+			model.addObject("apellidos", "");
+			model.addObject("email", "");
+			model.addObject("rol", "");
+			model.setViewName("deleteUser");
+		}
 		return model;
 	}
+
 	@RequestMapping(value = "/updatePassword", method = RequestMethod.POST)
 	public String updatePassword(@ModelAttribute("passwordVieja") String contrasennaVieja,
 			@ModelAttribute("passwordNueva") String contrasennaNueva, HttpServletRequest request) throws Exception {
@@ -68,93 +171,18 @@ public class UsuarioController {
 		HttpSession session = request.getSession(true);
 		String email = (String) session.getAttribute("emailSession");
 		String rolSession = (String) session.getAttribute("rolSession");
-		DAOEmpleado.updatePassword(email,passAntHex,passNuevaHex);
-		
-		if(rolSession.equals("admin")) {
+		DAOEmpleado.updatePassword(email, passAntHex, passNuevaHex);
+
+		if (rolSession.equals("admin")) {
 			pagina = "admin";
 		}
-		if(rolSession.equals("user")) {
+		if (rolSession.equals("user")) {
 			pagina = "user";
 		}
-		if(rolSession.equals("incid")) {
+		if (rolSession.equals("incid")) {
 			pagina = "incid";
 		}
 		return pagina;
 	}
-	
-	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
-	public String saveUser(@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
-			@ModelAttribute("email") String email, @ModelAttribute("rol") String rol) throws Exception {
-		Document empleado=new Document();
-		String contrasenna = EmpleadoHelper.generarContrasenna();
-		String emailLowerCase=email.toLowerCase();
-		empleado.put("email", emailLowerCase);
-		empleado.put("rol", rol);
-		empleado.put("nombre", nombre);
-		empleado.put("apellidos", apellidos);
-		empleado.put("contrasenna", contrasenna);
-		if(DAOEmpleado.insert(empleado)) {
-			EmpleadoHelper.sesionEmail(email, contrasenna);
-		}
-		
-		return "admin";
-	}
-	
-	@RequestMapping(value = "/editUser", method = RequestMethod.POST)
-	public String editUser(@ModelAttribute("nombre") String nombre, @ModelAttribute("apellidos") String apellidos,
-			@ModelAttribute("emailAntiguo") String emailAntiguo, @ModelAttribute("emailNuevo") String emailNuevo, @ModelAttribute("rol") String rol) {
-		Document empleado=new Document();
-		String emailLowerCase=emailNuevo.toLowerCase();
-		empleado.put("email", emailLowerCase);
-		empleado.put("rol", rol);
-		empleado.put("nombre", nombre);
-		empleado.put("apellidos", apellidos);
-		DAOEmpleado.update(emailAntiguo, empleado);
-		return "admin";
-	}
-	
-	@RequestMapping(value = "/searchUser", method = RequestMethod.POST)
-	public ModelAndView searchUser(ModelAndView model, @ModelAttribute("email") String email) {
-		String emailLowerCase=email.toLowerCase();
-		try {
-			Document empleado = DAOEmpleado.cargarEmpleado(emailLowerCase);
-			model.addObject("nombre", empleado.get("nombre"));
-			model.addObject("apellidos", empleado.get("apellidos"));
-			model.addObject("email", empleado.get("email"));
-			model.addObject("rol", empleado.get("rol"));
-			model.setViewName("updateUser");
-		}catch(Exception e){
-			model.addObject("nombre","");
-			model.addObject("apellidos", "");
-			model.addObject("email", "");
-			model.addObject("rol", "");
-			model.setViewName("updateUser");
-		}
-		return model;
-	}
-	
-	@RequestMapping(value = "/deleteSearchUser", method = RequestMethod.POST)
-	public ModelAndView deleteSearchUser(ModelAndView model, @ModelAttribute("email") String email) {
-		Document empleado = new Document();
-		String emailLowerCase=email.toLowerCase();
-		try {
-			empleado = DAOEmpleado.cargarEmpleado(emailLowerCase);
-			model.addObject("nombre", empleado.get("nombre"));
-			model.addObject("apellidos", empleado.get("apellidos"));
-			model.addObject("email", empleado.get("email"));
-			model.addObject("rol", empleado.get("rol"));
-			model.setViewName("deleteUser");
-		}catch(Exception e){
-			model.addObject("nombre", "");
-			model.addObject("apellidos", "");
-			model.addObject("email", "");
-			model.addObject("rol", "");
-			model.setViewName("deleteUser");
-		}
-		return model;
-	}
-	
-	
-	
 
 }
