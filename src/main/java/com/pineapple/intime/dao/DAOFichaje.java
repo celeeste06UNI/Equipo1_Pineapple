0886868e9edbb4edba1cd309977aeb07a5be6add
celeste <celeste@172.19.181.61>
2018-11-29 13:39:26 +0100

    formato fichaje

--- src/main/java/com/pineapple/intime/dao/DAOFichaje.java
+++ src/main/java/com/pineapple/intime/dao/DAOFichaje.java
@@ -127,11 +127,11 @@ public class DAOFichaje {
     		String fichaje="";
     		for (String keys : doc.keySet()) {
     			if(!keys.equals("_id")) {
-    				fichaje=fichaje+doc.get(""+keys)+" - ";
+    				fichaje=fichaje+doc.get(""+keys)+" ";
 					
 				}
     		}
-    		result.add((String) fichaje.substring(0, fichaje.length()-2));
+    		result.add((String) fichaje);
     	}
     	return result;
     	
